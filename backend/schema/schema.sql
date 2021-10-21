9f7370453e33e65e5e6e1ef7af45e3a1c61608dd
aphy18 <aphason1@.com>
2021-10-21 22:01:28 +0000

    refining seeds

--- backend/schema/schema.sql
+++ backend/schema/schema.sql
@@ -18,8 +18,7 @@ CREATE TABLE hobby (
   id SERIAL PRIMARY KEY,
   hobby_name VARCHAR(255) NOT NULL,
   level_of_expertise VARCHAR(255) NOT NULL,
-  minimum_spending_estimate VARCHAR(255) NOT NULL,
-  maximum_spending_estimate VARCHAR(255) NOT NULL,
+  my_spending_estimate VARCHAR(255) NOT NULL,
   amount_of_time_doing_hobby VARCHAR(255) NOT NULL,
   person_id INTEGER REFERENCES person(id) ON DELETE CASCADE
 );
--- backend/seeds/02_hobby.sql
+++ backend/seeds/02_hobby.sql
@@ -1,5 +1,15 @@
-INSERT INTO hobby (hobby_name, level_of_expertise, minimum_spending_estimate, maximum_spending_estimate, amount_of_time_doing_hobby, person_id)
-VALUES ('jump rope', 'advanced', '$12', '$50', '6 years', 1),
-('drawing', 'intermediate', '$20', '$100', '3 years', 2),
-('fishing', 'beginner', '$100', '$200', '5 months', 3),
-('biking', 'advanced', '$50', '$400', '8 years', 4),
+INSERT INTO hobby (hobby_name, level_of_expertise, my_spending_estimate, amount_of_time_doing_hobby, person_id)
+VALUES ('jump rope', 'advanced','$300', '6 years', 1),
+('baking', 'beginner', '$200', '4 months', 1),
+
+('drawing', 'intermediate', '$400', '3 years', 2),
+('jet skiing', 'advanced', '$1200', '5 years', 2),
+
+('fishing', 'beginner', '$600', '5 months', 3),
+('karate', 'intermediate', '$100/month', '9 months', 3),
+
+('biking', 'advanced', '$1000', '8 years', 4),
+('video games', 'advanced', '$4000', '2 years', 4),
+
+('video games', 'intermediate', '$650', '6 years', 5),
+('Ruby', 'beginner', '$0', '1 month', 5);
