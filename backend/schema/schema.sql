97e27f66b5f40b119e2447c9d775e7404a7b7214
aphy18 <aphason1@.com>
2021-10-21 20:37:41 +0000

    tables and adding seeds

--- backend/schema/schema.sql
+++ backend/schema/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS person CASCADE;
+DROP TABLE IF EXISTS hobby CASCADE;
+DROP TABLE IF EXISTS text_message CASCADE;
+
 CREATE TABLE person (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(255) NOT NULL,
@@ -17,5 +21,11 @@ CREATE TABLE hobby (
   minimum_spending_estimate VARCHAR(255) NOT NULL,
   maximum_spending_estimate VARCHAR(255) NOT NULL,
   amount_of_time_doing_hobby VARCHAR(255) NOT NULL,
+  person_id INTEGER REFERENCES person(id) ON DELETE CASCADE
+);
+
+CREATE TABLE text_message (
+  id SERIAL PRIMARY KEY,
+  text_message VARCHAR(255) NOT NULL,
   person_id INTEGER REFERENCES person(id) ON DELETE CASCADE
 );
---
+++ backend/seeds/01_person.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (first_name, last_name, person_address, person_email, person_password, person_gender, person_bio, date_of_membership)
+VALUES('Ben', 'Lents', '1234 Street', 'Ben12@gmail.com','1234', 'M', 'I like turtles', '2021-3-10 20:27:43.6928'),
+('Aphy', 'Aberham', '567 Street', 'Aphy@gmail.com','1234', 'M', 'I like ballons', '2020-5-19 9:14:30'),
+('Shivani', 'Adams', '8910 Street', 'Shivani@gmail.com', '1234', 'F', 'I am in Toronto', '2020-8-22 13:11:11'),
+('Alex', 'Madini', '12 Street', 'alexman@gmail.com', '1234', 'M', 'I love Javascript', '2019-12-02 7:30:01')
