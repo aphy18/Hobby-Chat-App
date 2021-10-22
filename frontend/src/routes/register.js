2a7d7a921e6d42a4b4b31a418362432ca7cf8a04
aphy18 <aphason1@gmail.com>
2021-10-22 18:12:39 -0400

    starting register page

--- frontend/src/routes/register.js
+++ frontend/src/routes/register.js
@@ -1,5 +1,18 @@
+import registerStyles from '../styles/Register.css'
+
 export default function Register() {
   return (
-   <h1>Register</h1>
+    <>
+    <div className="master-register-container">
+     <div className="register-container">
+      <h1 className="register">Register</h1>
+      <form className="register-form">
+        <input type="email" placeholder="email" required className="input-field"></input>
+        <input type="password" placeholder="password" required className="input-field"></input>
+        <button type="submit" className="form-button-submit">Submit</button>
+      </form>
+    </div>
+    </div>
+    </>
   )
 }
