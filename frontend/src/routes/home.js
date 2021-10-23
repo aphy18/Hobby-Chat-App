8556187171951369f6b979de9d600d5c50ad1213
aphy18 <aphason1@gmail.com>
2021-10-23 14:50:29 -0400

    formatting home about section

--- frontend/src/routes/home.js
+++ frontend/src/routes/home.js
@@ -4,17 +4,22 @@ export default function Home() {
   return (
     <>
     <div className="master-home-container">
-      <div className="home-container">
-   <h1 className="home">Home Page</h1>
+      <div className="header-container">
+   <h1 className="home">Welcome to Chattr</h1>
    </div>
    <div className="master-about-container">
-
+     <h2 className="about-us">About us</h2>
+     <div className="about-us-container">
+       <div className="about-box"></div>
+       <div className="about-box"></div>
+       <div className="about-box"></div>
+     </div>
    </div>
    <div className="master-features-container">
 
    </div>
    <div className="master-footer-container">
-     
+
    </div>
    </div>
    </>
