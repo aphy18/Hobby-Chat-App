8384279fadd44b71bc772b0cfccbe9af582030d9
aphy18 <aphason1@gmail.com>
2021-10-22 21:44:38 -0400

    making space for image

--- frontend/src/routes/register.js
+++ frontend/src/routes/register.js
@@ -5,22 +5,22 @@ export default function Register() {
     <>
     <div className="master-register-container">
      <div className="register-container">
-      <h1 className="register"><i>Register</i></h1>
+      <h1 className="register-header"><i>Register</i></h1>
       <form className="register-form">
         <div className="register-form-left-and-right">
         <div className="register-form-left">
-        <input type="text" placeholder="first name" required className="input-field"></input>
-        <input type="text" placeholder="last name" required className="input-field"></input>
-        <select className="input-field">
+        <input type="text" placeholder="first name" required className="register-input-field"></input>
+        <input type="text" placeholder="last name" required className="register-input-field"></input>
+        <select className="register-input-field">
           <option value="male">Male</option>
           <option value="female">Female</option>
           <option value="other">Other</option>
         </select>
         </div>
         <div className="register-form-right">
-        <input type="email" placeholder="email" required className="input-field"></input>
-        <input type="password" placeholder="password" required className="input-field"></input>
-        <input type="password" placeholder="re-enter password" required className="input-field"></input>
+        <input type="email" placeholder="email" required className="register-input-field"></input>
+        <input type="password" placeholder="password" required className="register-input-field"></input>
+        <input type="password" placeholder="re-enter password" required className="register-input-field"></input>
         </div>
         </div>
         <div className="button-container">
