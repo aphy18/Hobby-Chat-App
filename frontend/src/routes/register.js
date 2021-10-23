ee540ac364cff95e7341a0158057265357d70e07
aphy18 <aphason1@gmail.com>
2021-10-22 21:16:36 -0400

    register form formatting

--- frontend/src/routes/register.js
+++ frontend/src/routes/register.js
@@ -7,17 +7,22 @@ export default function Register() {
      <div className="register-container">
       <h1 className="register"><i>Register</i></h1>
       <form className="register-form">
+        <div className="register-form-left-and-right">
+        <div className="register-form-left">
         <input type="text" placeholder="first name" required className="input-field"></input>
         <input type="text" placeholder="last name" required className="input-field"></input>
-        <select>
+        <select className="input-field">
           <option value="male">Male</option>
           <option value="female">Female</option>
           <option value="other">Other</option>
         </select>
-        <input type="text" placeholder="address" required className="input-field"></input>
+        </div>
+        <div className="register-form-right">
         <input type="email" placeholder="email" required className="input-field"></input>
         <input type="password" placeholder="password" required className="input-field"></input>
         <input type="password" placeholder="re-enter password" required className="input-field"></input>
+        </div>
+        </div>
         <div className="button-container">
       <button type="submit" className="form-button-submit">Submit</button>
       </div>
