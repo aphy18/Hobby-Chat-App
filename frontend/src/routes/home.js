24d9c78ad5f9df844a4f4de93c833d9c9f04b322
aphy18 <aphason1@gmail.com>
2021-10-23 20:36:36 -0400

    refining home containers

--- frontend/src/routes/home.js
+++ frontend/src/routes/home.js
@@ -4,19 +4,19 @@ export default function Home() {
   return (
     <>
     <div className="master-home-container">
-      <div className="header-container">
-   <h1 className="home">Welcome to Chattr</h1>
+   <div className="home-header-container">
+   <h2 className="home-header">Welcome to chatter</h2>
    </div>
-   <div className="master-about-container">
-     <h2 className="about-us">About us</h2>
+   <div className="the-about-container">
+   <h2 className="about-us">About us</h2>
      <div className="about-us-container">
        <div className="about-box"></div>
        <div className="about-box"></div>
        <div className="about-box"></div>
      </div>
    </div>
-   <div className="master-features-container">
-    <h2 className="features">Features</h2>
+   <div className="main-features-container">
+   <h2 className="features">Features</h2>
     <div className="features-container">
     <div className="features-image"></div>
       <div className="features-description"></div>
