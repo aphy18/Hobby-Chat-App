d6d74f2e41377ac0eb8da7d0ee46f4dee341ed97
aphy18 <aphason1@gmail.com>
2021-10-24 21:23:34 -0400

    working on profile page

--- frontend/src/routes/profile.js
+++ frontend/src/routes/profile.js
@@ -1,5 +1,16 @@
 export default function Profile(){
   return (
+    <>
     <h1>Profile Page</h1>
+    <div className="profile-container">
+      <p className="profile-item">Username:</p>
+      <p className="profile-item">First Name:</p>
+      <p className="profile-item">Last Name:</p>
+      <p className="profile-item">Gender:</p>
+      <p className="profile-item">Email:</p>
+    </div>
+    <button>Edit</button>
+    <button>Change Password</button>
+    </>
   )
 }
