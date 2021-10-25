7d76c09d982045ad787e62687aca17ad4f91cd6f
aphy18 <aphason1@gmail.com>
2021-10-25 13:46:57 -0400

    made custom hook

---
+++ frontend/src/customHooks/useForm.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export default function useForm(callback, initValues={}) {
+  const [values, setValues] = useState(initValues)
+
+  const handleChange = e => {
+    const { name, value } = e.target
+
+    console.log('name -->', name);
+    console.log('value -->', value);
+    console.log('values obj -->', values);
+
+    setValues({
+      ...values,
+      [name]:values
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    callback()
+  }
+
+
+  return { values, setValues, handleChange, handleSubmit }
+}
--- frontend/src/routes/register.js
+++ frontend/src/routes/register.js
@@ -1,7 +1,11 @@
-import registerStyles from '../styles/Register.css'
-import bodyStyles from '../styles/Body.css'
+import '../styles/Register.css'
+import '../styles/Body.css'
+import useForm from '../customHooks/useForm'
 
 export default function Register() {
+  
+  
+
   return (
     <>
     <div className="master-register-container">
@@ -12,20 +16,20 @@ export default function Register() {
       <form className="register-form">
         <div className="register-form-left-and-right">
         <div className="register-form-left">
-        <input type="text" placeholder="username" required className="register-input-field"></input>
-        <input type="text" placeholder="first name" required className="register-input-field"></input>
-        <input type="text" placeholder="last name" required className="register-input-field"></input>
-        <select className="register-input-field" id="gender-select">
+        <input type="text" placeholder="username" required className="register-input-field" value="username"></input>
+        <input type="text" placeholder="first name" required className="register-input-field" value="first_name"></input>
+        <input type="text" placeholder="last name" required className="register-input-field" value="last_name"></input>
+        <select className="register-input-field" id="gender-select" value="gender">
           <option value="male">Male</option>
           <option value="female">Female</option>
           <option value="other">Other</option>
         </select>
         </div>
         <div className="register-form-right">
-        <input type="text" placeholder="address" required className="register-input-field"></input>
-        <input type="email" placeholder="email" required className="register-input-field"></input>
-        <input type="password" placeholder="password" required className="register-input-field"></input>
-        <input type="password" placeholder="re-enter password" required className="register-input-field"></input>
+        <input type="text" placeholder="address" required className="register-input-field" value="address"></input>
+        <input type="email" placeholder="email" required className="register-input-field" value="email"></input>
+        <input type="password" placeholder="password" required className="register-input-field" value="password"></input>
+        <input type="password" placeholder="re-enter password" required className="register-input-field" value="password_confirm"></input>
         </div>
         <span className="register-image"><i class="fa-solid fa-registered"></i></span>
         <span className="register-image"><i class="fa-solid fa-user"></i></span>
