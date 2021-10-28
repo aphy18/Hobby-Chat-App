92662aa2baccba28e01e795bd9f901a27f7bab68
aphy18 <aphason1@gmail.com>
2021-10-28 14:27:06 -0400

    editing format

--- frontend/src/routes/profile.js
+++ frontend/src/routes/profile.js
@@ -13,16 +13,23 @@ export default function Profile() {
       <div className="profile-picture">
        <span><i class="fas fa-user"></i></span>
       </div>
+      <div className="user-info">
         <p className="profile-item">Username: {userObj.username}</p>
         <p className="profile-item">First Name: {userObj.first_name}</p>
         <p className="profile-item">Last Name: {userObj.last_name}</p>
         <p className="profile-item">Gender: {userObj.person_gender}</p>
         <p className="profile-item">Address: {userObj.person_address}</p>
         <p className="profile-item">Email: {userObj.person_email}</p>
+      </div>
+      <div className="bio-container">
+        <p className="bio-header">Bio:</p>
+        <p className="bio">Hello i am whale i love whales</p>
+        {/* <textarea className="bio" placeholder="Talk a bit about yourself"></textarea> */}
+      </div>
       <div className="profile-button-container">
-      <button>Edit</button>
-      <button>Change Password</button>
-      <button>Upload Profile Picture</button>
+      <button className="profile-button">Edit</button>
+      <button className="profile-button">Change Password</button>
+      <button className="profile-button">Upload Profile Picture</button>
       </div>
       </form>
       </div>
