0e55777e818b434e59700fd3e5b05c1b4f44bd37
aphy18 <aphason1@gmail.com>
2021-10-29 13:52:23 -0400

    styling message box

--- frontend/src/components/MessageOption.js
+++ frontend/src/components/MessageOption.js
@@ -1,9 +1,11 @@
-import '../styles/MessageOptions/css'
+import '../styles/MessageOptions.css'
 
 export default function MessageOption() {
   return (
     <div className="message-option-container">
-
+      <span className="message-option-img">Image</span>
+      <a href='/#' className="message-option-click"><i>Click to open messages</i></a>
+      <span className="message-option-username">To: Username</span>
     </div>
   )
 }
