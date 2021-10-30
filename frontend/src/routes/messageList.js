1317c7c5bcbb22d6cb9133c0acb4b1ddd63c31d7
aphy18 <aphason1@gmail.com>
2021-10-29 20:46:50 -0400

    styling to list of users, got data from db

--- frontend/src/routes/messageList.js
+++ frontend/src/routes/messageList.js
@@ -5,24 +5,46 @@ import MessageOption from '../components/MessageOption';
 import axios from 'axios';
 
 export default function MessageList() {
+
+  const [data,setData] = useState([])
   
   async function getAllUserData() {
     const userData = await axios.get(`http://localhost:8080/messageList`);
     console.log('this is user data ---->', userData)
+    setData(userData.data)
 
   }
-
+  
   useEffect(() => {
     getAllUserData()
     
   }, [])
-  
 
+  console.log('hi im data --->', data)
+  
+  
+  
+  const users = data.map(user => {
+    return (
+      <>
+      <div className="message-list-container">
+        <MessageOption />
+      </div>
+      </>
+    )
+  })
+  
   return (
     <>
-    <div className="message-list-container">
-      <MessageOption />
-    </div>
+    {users}
     </>
   )
+  
+
+
+  
+   
+    
+  
+
 }
