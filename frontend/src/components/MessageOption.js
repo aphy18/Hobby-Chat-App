319a60b4fc6b17c980e2137ebad9458e8634c283
aphy18 <aphason1@gmail.com>
2021-10-30 00:17:28 -0400

    message list page

--- frontend/src/components/MessageOption.js
+++ frontend/src/components/MessageOption.js
@@ -5,7 +5,7 @@ export default function MessageOption(props) {
     <div className="message-option-container">
       <span className="message-option-img">Image</span>
       <div className="message-option-click-container">
-      <a href='/#' className="message-option-click"><i>Click to open messages</i></a>
+      <a href='/' className="message-option-click"><i>Click to open messages</i></a>
       <i class="fas fa-comments"></i>
       </div>
       <span className="message-option-username">To: {props.user.username}</span>
