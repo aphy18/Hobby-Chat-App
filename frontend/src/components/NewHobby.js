8bb6b00114dcd23e1ba79252491b26af38b5bdef
aphy18 <aphason1@gmail.com>
2021-10-30 15:48:43 -0400

    styling new hobby page

--- frontend/src/components/NewHobby.js
+++ frontend/src/components/NewHobby.js
@@ -9,7 +9,7 @@ export default function NewHobby() {
     <h1>New Hobby</h1>
     <div className='new-hobby-master-container'>
       <div className='new-hobby-container'>
-        <form>
+        <form className="new-hobby-form">
           <input name="hobby_name"type="text"></input>
           <input name="level_of_expertise"type="text"></input>
           <input name="my_spending_estimate"type="text"></input>
