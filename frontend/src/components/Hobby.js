5b4b4b828f65632652154e0d53d3bfb812f2cc08
aphy18 <aphason1@gmail.com>
2021-10-31 20:36:47 -0400

    styling

--- frontend/src/components/Hobby.js
+++ frontend/src/components/Hobby.js
@@ -1,13 +1,15 @@
+
+
 export default function Hobby(props){
 
   const getHobbyData = props.hobbyData.map(hobby => {
     return (
       <>
         <div className="hobby-info">
-          <p>Hobby Name: {hobby.hobby_name}</p>
-          <p>Skill Level: {hobby.level_of_expertise}</p>
-          <p>Time Doing Hobby: {hobby.amount_of_time_doing_hobby}</p>
-          <p>Spending Estimate: {hobby.my_spending_estimate}</p>
+          <p className="hobby-item">Hobby Name: {hobby.hobby_name}</p>
+          <p className="hobby-item">Skill Level: {hobby.level_of_expertise}</p>
+          <p className="hobby-item">Time Doing Hobby: {hobby.amount_of_time_doing_hobby}</p>
+          <p className="hobby-item">Spending Estimate: {hobby.my_spending_estimate}</p>
         </div>
       </>
     )
