5d4c4aabe60cdbfe819c36b79dd39118af0699d8
aphy18 <aphason1@gmail.com>
2021-12-02 19:21:02 -0500

    successfully emit and receive messages from socket

--- frontend/src/routes/message.js
+++ frontend/src/routes/message.js
@@ -7,12 +7,18 @@ import useForm from '../customHooks/useForm';
 
 export default function Message() {
 
+  const socket = io('http://localhost:8080')
+    socket.on('connection', () => {
+       console.log('connected')
+    })
+
   const { values, setValues, handleChange, handleSubmit } = useForm(sendMessage)
   const arr = [];
+  const storeMessages = [];
+  const [message,setMessage] = useState(false)
   const [data,setData] = useState([]);
   const splitPathName = parseInt(window.location.pathname.split('/')[2]);
-  let userObj = JSON.parse(localStorage.getItem('user'))
-  console.log('SPLIT PATH -->',splitPathName);
+  let userObj = JSON.parse(localStorage.getItem('user'));
 
   async function request(){
     const getFriendData = await axios.get('http://localhost:8080/message')
@@ -23,7 +29,7 @@ export default function Message() {
     request()
   },[])
 
-  console.log('data',data)
+  // console.log('data',data)
 
   data.map(obj => {
     if (obj.username === userObj.username && obj.receiver_id === splitPathName) {
@@ -31,19 +37,36 @@ export default function Message() {
     }
   })
 
-  console.log('arr 37 -->',arr)
+  // console.log('arr 37 -->',arr)
 
   async function sendMessage(){
-    const saveMessage = await axios.post('http://localhost:8080/message', { values,arr })
-    console.log('saving message',saveMessage)
+    await axios.post('http://localhost:8080/message', { values,arr })
+    setValues({})
   }
- 
+
+  async function emitMessage(){
+    await socket.emit('message',{ handler: userObj.username, message: values.message })
+    setMessage(true)
+  }
+
+  
+  function receiveMessage(){
+    socket.on('message', data => {
+      console.log('this is socket messsage data',data)
+    })
+  }
+
+  useEffect(() => {
+    receiveMessage()
+  },[message])
+  
+  
   return (
     <div className="master-message-container">
       <div className="message-form-container">
-        <form className="message-form">
+        <form className="message-form" onSubmit={handleSubmit}>
          <textarea name="message" value={values.message} className="input-area" type="text" placeholder="message" onChange={handleChange}></textarea>
-         <button className="message-page-button" onClick={handleSubmit}>Send</button>
+         <button className="message-page-button" onClick={() => emitMessage()}>Send</button>
         </form>
       </div>
     <div className="chat-log">
