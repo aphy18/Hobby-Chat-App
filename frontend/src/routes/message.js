f0edd1d060a1c308b1d0ab4e0ee32595d7f44976
aphy18 <aphason1@gmail.com>
2021-12-04 09:35:53 -0500

    working on messaging feature (saving this state)

--- frontend/src/routes/message.js
+++ frontend/src/routes/message.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import '../styles/Message.css'
 import io from 'socket.io-client';
 import axios from 'axios'
 import useForm from '../customHooks/useForm';
+import ReactDOM from 'react-dom';
 
 export default function Message() {
 
@@ -12,17 +13,19 @@ export default function Message() {
        console.log('connected')
     })
 
+  const inputRef = useRef()
   const { values, setValues, handleChange, handleSubmit } = useForm(sendMessage)
   const arr = [];
   const storeMessages = [];
+  const [socketInfo,setSocketInfo] = useState({})
   const [message,setMessage] = useState(false)
-  const [data,setData] = useState([]);
+  const [getData,setGetData] = useState([]);
   const splitPathName = parseInt(window.location.pathname.split('/')[2]);
   let userObj = JSON.parse(localStorage.getItem('user'));
 
   async function request(){
     const getFriendData = await axios.get('http://localhost:8080/message')
-    setData(getFriendData.data)
+    setGetData(getFriendData.data)
   }
 
   useEffect(() => {
@@ -31,7 +34,7 @@ export default function Message() {
 
   // console.log('data',data)
 
-  data.map(obj => {
+  getData.map(obj => {
     if (obj.username === userObj.username && obj.receiver_id === splitPathName) {
       arr.push(obj)
     }
@@ -49,27 +52,38 @@ export default function Message() {
     setMessage(true)
   }
 
-  
-  function receiveMessage(){
-    socket.on('message', data => {
-      console.log('this is socket messsage data',data)
-    })
-  }
-
   useEffect(() => {
     receiveMessage()
   },[message])
+
   
+  function receiveMessage(){
+    socket.on('message', (data) => {
+      console.log('this is socket messsage data',data)
+      const userMessage = React.createElement('p',{class: 'user-message'},`${data.handler}: ${data.message}`)
+      console.log('USER MESSAGE',userMessage)
+      ReactDOM.render(userMessage,document.getElementById('chat-log'))
+    })
+  }
+
+  console.log(storeMessages)
+  
+
+
+  function focus(){
+    console.log(inputRef.current.value)
+  }
   
   return (
     <div className="master-message-container">
       <div className="message-form-container">
         <form className="message-form" onSubmit={handleSubmit}>
-         <textarea name="message" value={values.message} className="input-area" type="text" placeholder="message" onChange={handleChange}></textarea>
+         <textarea ref={inputRef} name="message" value={values.message} className="input-area" type="text" placeholder="message" onChange={handleChange}></textarea>
          <button className="message-page-button" onClick={() => emitMessage()}>Send</button>
+         <button onClick={focus}>button</button>
         </form>
       </div>
-    <div className="chat-log">
+    <div className="chat-log" id="chat-log">
       {/* make a seperate variable for displaying messages, map over an array of
       messages and put them in the chat log (going to need to use socket io for the live messaging effect) */}
     </div>
