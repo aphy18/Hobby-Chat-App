376443a4967fae6c53e64734cff76b98db89e908
aphy18 <aphason1@gmail.com>
2021-12-26 21:20:51 -0500

    still styling popup

--- frontend/src/components/LoginPopup.js
+++ frontend/src/components/LoginPopup.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import '../styles/Popup.css'
+import Aos from "aos";
+import { useEffect } from 'react';
 
 export default function LoginPopup(props) {
 
+  useEffect(() => {
+    Aos.init({duration: 600})
+  })
+
 
     return (props.trigger) ? (
-      <div className='pop-up-container'>
+      <div className='pop-up-container' data-aos="zoom-in-down">
         <button className='pop-up-button' onClick={() => props.setTrigger(false)}>Close</button>
         <p>You have logged in! Click <a href="/">here</a> to go to home page</p>
       </div>
@@ -13,17 +19,3 @@ export default function LoginPopup(props) {
   
   
 }
-
-
-// {(user !== null) ?
-//   <>
-//     <span className="nav-item" onClick={() => logout()}><i class="fas fa-sign-in-alt"></i></span>
-//     <a href="/friends" className="nav-item"><i class="fas fa-user-friends"></i></a>
-//     <a href="/requests" className="nav-item"><i class="fas fa-inbox"></i></a>
-//     <a href={`/profile/${userObj.id}`} className="nav-item"><i class="fas fa-user-cog"></i></a>
-//   </>
-//   : <>
-//     <a href='/register' className="nav-item fixed"><i class="fas fa-user-plus fixed"></i></a>
-//     <a href='/login' className="nav-item"><i class="fas fa-sign-in-alt"></i></a>
-//   </>}
-// </ul>
