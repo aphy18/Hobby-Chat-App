d510f62d9574003683dc6b65f8eea6669714a31c
aphy18 <aphason1@gmail.com>
2021-12-27 20:17:09 -0500

    options post registration

--- frontend/src/components/LoginPopup.js
+++ frontend/src/components/LoginPopup.js
@@ -3,18 +3,15 @@ import '../styles/Popup.css'
 import Aos from "aos";
 import { useEffect } from 'react';
 
-export default function LoginPopup(props) {
+export default function LoginPopup() {
 
   useEffect(() => {
     Aos.init({duration: 500})
   })
-
-
-    return (
+    
+  return (
       <div className='pop-up-container' data-aos="zoom-in">
         <p className='logged-in'>You have logged in! Click <a href="/">here</a> to go to home page</p>
       </div>
     )
-  
-  
 }
--- frontend/src/components/RegisterPopup.js
+++ frontend/src/components/RegisterPopup.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../styles/Popup.css'
+import Aos from "aos";
+import { useEffect } from 'react';
+
+// make a /setup page
 
 export default function RegisterPopup() {
   return (
- 
-      <p></p>
-  
-  )
+      <>
+      <div className='pop-up-container'>
+      <p>Click <a href="/login">here</a> to login or take a minute to set up the rest of your account <a href='/#'>here</a></p>
+      </div>
+      </>
+    )
 }
